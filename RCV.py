e92e65918e63ac09bcdb998d83cdadfd36ca8445
Timothy Colledge <51029752+timmcoll03@users.noreply.github.com>
2021-11-09 20:15:29 -0500

    init

---
+++ RCV.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import math as m 
+
+
+def dataRetrive(path):
+    df = pd.read_csv(path)
+    df = df.drop(["Name"], axis = 1)
+    return df
+
+def checkVotes(results):
+    print(results)
+    if results[0] >= sum(results)/2+1: 
+        print("The winner is candidate "+ str(results.keys()[0] + " with " + str(results[0])+" first-place votes"))
+        return True
+    elif results[0] == results[1]:
+        print("No winner, there was a tie")
+        return True
+    else:   
+        return False
+
+def reRank(results,df):
+    for y in range(len(df.columns)-1):
+        for x in range(len(df[df.columns[0]])):   
+            if results.keys()[len(results)-1] == df[df.columns[y]][x]:
+                    df[df.columns[y]][x] = df[df.columns[y+1]][x]
+                    df[df.columns[y+1]][x] = np.NaN
+
+    for x in range(len(df[df.columns[0]])):   
+        if results.keys()[len(results)-1] == df[df.columns[len(df.columns)-1]][x]:
+                df[df.columns[len(df.columns)-1]][x] = np.NaN
+
+    for y in range(len(df.columns)-2,0,-1):
+        for x in range(len(df[df.columns[0]])): 
+            if isinstance(df[df.columns[y]][x], str) != True:
+                if m.isnan(df[df.columns[y]][x]) == True:
+                    df[df.columns[y]][x] = df[df.columns[y+1]][x]
+
+def checkTies(results,df):
+    for x in range(len(results)-1):
+        if results[len(results)-1] == results[len(results)-1-x]:
+            for y in range(len(df[df.columns[0]])):
+                if df[df.columns[0]][y] == results.keys()[len(results)-1-x]:
+                    df[df.columns[0]][y] = results.keys()[len(results)-1]
+
+def RCV(df):
+    for x in range(len(df.columns)-1):
+        results = df["Choice 1"].value_counts()
+        tryed = checkVotes(results)
+        if tryed == False:
+            checkTies(results,df)
+            reRank(results,df)
+        else:
+            break
+        if x == len(df.columns)-2:
+            print("This election has no winner.")
+
+def main():
+    path = input("Enter the filename: ")
+    df = dataRetrive(path)
+    RCV(df)
+
+main()
